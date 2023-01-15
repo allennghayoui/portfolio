ae4df3383f5f4841b0d5a4f723e433efbe428eb9
Allen Nghayoui <allen.nghayoui@gmail.com>
2023-01-15 18:56:51 +0200

    fixed typo in education section and deployed to firebase

--- src/data/major.js
+++ src/data/major.js
@@ -5,7 +5,7 @@ export default [
         date: '2021 - Present',
         details: ['Expected to graduate in Spring 2024', 'GPA: 3.98'],
         coursesCompleted: ['Object-Oriented Design', 'Data Structures', 'Pro Design and Data Abstraction'],
-        currentlyEnrolled: ['Principles of Database Systems', 'DataCommunications and Computer Networks',
+        currentlyEnrolled: ['Principles of Database Systems', 'Data Communications and Computer Networks',
                             'Analysis of Algorithms'],
     },
 
