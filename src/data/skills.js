7dee3038ce7fe605be0fe690bdf47f503f6f484c
Allen Nghayoui <allen.nghayoui@gmail.com>
2023-01-17 16:22:15 +0200

    added to the skills section

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,4 +1,4 @@
-export const technicalSkills = ['C++', 'C#/.NET', 'Python', 'HTML/CSS', 'Javascript', 'React']
+export const technicalSkills = ['C++', 'C#/.NET', 'Python', 'SQL', 'Networking', 'HTML/CSS', 'Javascript', 'React']
 
 export const softSkills = ['Problem Solving', 'Anayltical', 'Communication', 'Time Management', 'Attention to Detail']
 
