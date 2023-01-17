bf52321a94c87619ffebb698ff4f91d08185696c
Allen Nghayoui <allen.nghayoui@gmail.com>
2023-01-17 13:20:03 +0200

    fixed typo in courses list

--- src/data/major.js
+++ src/data/major.js
@@ -4,7 +4,7 @@ export default [
         location: 'Notre Dame University Louaize - Zouk Mosbeh',
         date: '2021 - Present',
         details: ['Expected to graduate in Spring 2024', 'GPA: 3.98'],
-        coursesCompleted: ['Object-Oriented Design', 'Data Structures', 'Pro Design and Data Abstraction'],
+        coursesCompleted: ['Object-Oriented Design', 'Data Structures', 'Program Design and Data Abstraction'],
         currentlyEnrolled: ['Principles of Database Systems', 'Data Communications and Computer Networks',
                             'Analysis of Algorithms'],
     },
